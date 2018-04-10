5026810fb6dcfbcba3724eb8b09d912af5d46630
Vin Ratford <vratford@gmail.com>
2018-04-10 16:32:55 -0700

    Updated to Xcode 9.3

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,48 @@
 //
 
 import UIKit
+import AVFoundation //Adds audio toolbox library
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var soundArray = ["note1","note2","note3","note4","note6","note6","note7","All Shook Up Short", "Dreamweaver short"]
+    var audioPlayer : AVAudioPlayer!
+    var soundFileName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+         playSound(soundFileName: soundArray[7])
+        
     }
 
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+    
+        playSound(soundFileName: soundArray[sender.tag - 1])
+       
+    }
+    
+    func playSound(soundFileName: String) {
         
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
+        do {
+            
+            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
+            
+            //            guard let player = player else { return }
+            //
+            //            player.prepareToPlay()
+            //            player.play();
+        }
+            
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
         
     }
     
-  
-
 }
-
---
+++ Xylophone/Sound Files.swift
@@ -0,0 +1,9 @@
+//
+//  Sound Files.swift
+//  Xylophone
+//
+//  Created by Vincent Ratford on 1/4/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
